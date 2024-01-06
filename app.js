c1dbe29aa856c691d34bbaf0790d02e0f1113d77
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-05 22:04:21 -0500

    Add todo Functionality working

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("I am initalized!");
+const form = document.querySelector("#add-todo");
+const todoInput = document.querySelector('input[name="todo"]');
+const todoContent = document.querySelector("#todoContent");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+
+  todoContent.append(newTodo);
+});
