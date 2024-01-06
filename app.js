334eee78e181558af2add3d94660c583a7b4bb8c
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-06 01:42:42 -0500

    localStorage persisting

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ const form = document.querySelector("#add-todo");
 const todoInput = document.querySelector('input[name="todo"]');
 const todoContent = document.querySelector("#todoContent");
 const removeButtons = document.querySelectorAll("ul button");
+let todoArr = JSON.parse(localStorage.getItem("todos")) || [];
+
+for (let todo of todoArr) {
+  const newTodoDiv = document.createElement("div");
+  const newTodo = document.createElement("li");
+  const removeBtn = document.createElement("button");
+
+  const newLiBtn = newTodoDiv.appendChild(newTodo);
+  newTodo.innerText = todo;
+  removeBtn.innerText = "X";
+
+  newTodoDiv.appendChild(newLiBtn);
+  newTodoDiv.appendChild(removeBtn);
+
+  newTodoDiv.classList.add("liBtn");
+  todoContent.appendChild(newTodoDiv);
+}
+
+// console.log(localStorage);
 
 todoContent.addEventListener("click", function (e) {
   if (e.target.tagName === "BUTTON") {
     e.target.parentElement.remove();
+    // console.log(e.target.parentElement.querySelector("li").innerText);
+    // localStorage.clear();
   } else if (e.target.tagName === "LI") {
     e.target.classList.toggle("crossed");
   }
@@ -16,18 +37,22 @@ form.addEventListener("submit", function (e) {
   const newTodoDiv = document.createElement("div");
   const newTodo = document.createElement("li");
   const removeBtn = document.createElement("button");
-  newTodo.innerText = todoInput.value;
-  removeBtn.innerText = "X";
 
   if (!todoInput.value) {
     return;
   } else {
     const newLiBtn = newTodoDiv.appendChild(newTodo);
+    newTodo.innerText = todoInput.value;
+    removeBtn.innerText = "X";
+
     newTodoDiv.appendChild(newLiBtn);
     newTodoDiv.appendChild(removeBtn);
 
     newTodoDiv.classList.add("liBtn");
     todoContent.appendChild(newTodoDiv);
+
+    todoArr.push(todoInput.value);
+    localStorage.setItem("todos", JSON.stringify(todoArr));
 
     todoInput.value = "";
   }
