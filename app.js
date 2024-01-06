1de2167658f8da507d3633125ae0385bc283b366
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-05 23:22:49 -0500

    Remove functionality implemented

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const form = document.querySelector("#add-todo");
 const todoInput = document.querySelector('input[name="todo"]');
 const todoContent = document.querySelector("#todoContent");
+const removeButtons = document.querySelectorAll("ul button");
+
+todoContent.addEventListener("click", function (e) {
+  if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+  }
+});
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  const newTodoDiv = document.createElement("div");
   const newTodo = document.createElement("li");
+  const removeBtn = document.createElement("button");
   newTodo.innerText = todoInput.value;
+  removeBtn.innerText = "X";
 
-  todoContent.appendChild(newTodo);
+  const newLiBtn = newTodoDiv.appendChild(newTodo);
+  newTodoDiv.appendChild(newLiBtn);
+  newTodoDiv.appendChild(removeBtn);
+  console.log(newTodoDiv);
+
+  newTodoDiv.classList.add("liBtn");
+  todoContent.appendChild(newTodoDiv);
 
   todoInput.value = "";
 });
